46dc6b3e330dd33c823b22ce2aaa9b3dd73f1e3a
jokicc123 <a0903010911@gmail.com>
2025-01-08 20:53:39 +0800

    Update資料類型

--- Assets/Scripts/Class_1_Variable.cs
+++ Assets/Scripts/Class_1_Variable.cs
@@ -6,7 +6,7 @@
 //引用unity游戲引擎的函示庫 (開發遊戲的程式集合,命名空間)
 using UnityEngine;
 // 修飾詞 類別關鍵字(腳本)類別名稱 腳本名稱
-public class Class_1_variable
+public class Class_1_variable : MonoBehaviour
 
 {
     //程式內的括號都是成對出現的()[]{}<>''""
@@ -20,7 +20,10 @@ public class Class_1_variable
     //變數資料類型 變數名稱;
     //資料類型:告訴記憶體可以放入那些資料，例如:整數 int 可以放沒有小數值的數值
     //變數名稱:習慣用小寫開頭名稱，駝峰式命名，規則與腳本名稱相同
-    int coin;
+    //公開: 允許其他類別存取、顯示在屬性面板上 public
+    //私人: 禁止其他類別存取、隱藏在屬性面板上 Private(預設)
+    public int coin =500;
+    private int level =3;
 }
 
 //類別外，不要將程式寫在這裡
---
+++ Assets/Scripts/Class_2_DataType.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+//三條斜線是摘要(XML語言)用來做簡短說明、可加在類別與變數上方
+/// <summary>
+/// 認識資料類型
+/// </summary>
+
+public class Class_2_DataType :MonoBehaviour
+{
+    //變數語法:
+    //修飾詞 資料類型 變數名稱 指定 預設值;
+    //四大腸用資料類型
+    //整數:儲存沒有小數點的數值 int
+    //浮點數:儲存有小數點的數值 float
+    //字串:儲存文字，必須使用雙引號 string
+    //布林直:儲存正與反 bool只有兩種值 ture與false
+    public int count = 7;
+    public float moveSpeed = 3.5f; //浮點數必須添加f後綴
+    public float turnSpeed = 3.5F; // 大小F皆可
+    public string character = "蓋倫";
+    public bool isDead = false;    //是否死亡: 否
+    public bool gameOver = true;   //是否死亡: 是
+}
