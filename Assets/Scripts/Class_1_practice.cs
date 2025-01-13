742666cf1688cf536c708a78921bd881e796dc7f
jokicc123 <a0903010911@gmail.com>
2025-01-13 19:13:27 +0800

    作業練習完成

---
+++ Assets/Scripts/Class_1_practice.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Class_1_practicet : MonoBehaviour
+{
+    public int price = 999;
+    private float Jumpheight = 77.5f;
+    public string weapon = "三項之力";
+    private bool getDragonsoul = true;
+
+}
+ 
---
+++ Assets/Scripts/Class_2_practice.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Class_2_practice : MonoBehaviour
+{
+  public float MoveSpeed =7.7f;
+  public double coin =0.12345f;
+  public decimal lv = 1.123456789m;
+}
