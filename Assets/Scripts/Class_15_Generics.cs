98a513adb5ccd493b1e85d80c8ce589e52244ad6
jokicc123 <a0903010911@gmail.com>
2025-03-12 21:41:53 +0800

    Update資料結構與佇列

--- Assets/Scripts/Class_15_Generics.cs
+++ Assets/Scripts/Class_15_Generics.cs
@@ -1,26 +1,50 @@
 ﻿using UnityEngine;
-namespace chang.Class_15 
+namespace chang.Class_15
 {
     /// <summary>
     /// 泛型
     /// </summary>
     public class Class_15_Generics : MonoBehaviour
     {
+       
 
         private void Awake()
         {
+
+            #region  不使用泛型
             int numberA = 7, numberB = 9;
             LogSystem.LogwithColor($"數字A與B:{numberA}|{numberB}", "#f93");
             SwapNumber(ref numberA, ref numberB);
             LogSystem.LogwithColor($"數字A與B:{numberA}|{numberB}", "#f93");
-           char charA ='嗨', charB = '嘿';
+            char charA = '嗨', charB = '嘿';
             LogSystem.LogwithColor($"字元A與B:{charA}|{charB}", "#3f3");
-            SwapChar (ref charA, ref charB);
+            SwapChar(ref charA, ref charB);
             LogSystem.LogwithColor($"字元A與B:{charA}|{charB}", "#3f3");
-           object objA = 3.5f, objB = 7.7f;
+            object objA = 3.5f, objB = 7.7f;
             LogSystem.LogwithColor($"物件A與B:{objA}|{objB}", "#3f3");
             SwapObject(ref objA, ref objB);
             LogSystem.LogwithColor($"物件A與B:{objA}|{objB}", "#3f3");
+
+            #endregion
+           bool boolA = true, boolB = false;
+            LogSystem.LogwithColor($"物件A與B:{boolA}|{boolB}", "#9f9");
+            Swap<bool>(ref boolA, ref boolB);
+            LogSystem.LogwithColor($"物件A與B:{boolA}|{boolB}", "#9f9");
+            byte byteA = 1, byteB = 9;
+            LogSystem.LogwithColor($"物件A與B:{objA}|{objB}", "#3f3");
+            Swap<byte>(ref byteA, ref byteB);
+            LogSystem.LogwithColor($"物件A與B:{objA}|{objB}", "#3f3");
+
+            var player1 = new DataPlayer<int>();
+            player1.data = 99;
+            player1.LogData(123);
+            
+            var player2 = new DataPlayer<string>();
+            player2.data = "玩家2號";
+            player2.LogData("哈囉");
+
+
+
         }
 
 
@@ -29,20 +53,21 @@ namespace chang.Class_15
         /// </summary>
         /// <param name="a"></param>
         /// <param name="b"></param>
+        #region 方法區域
         public void SwapNumber(ref int a, ref int b)
         {
             int temp = a;    //將第一個數字放去旁邊(暫存)
             a = b;           //將第二個放到第一個數字內
             b = temp;       //將旁邊的數字放到第二個數字 
-           
+
         }
 
 
         public void SwapChar(ref char a, ref char b)
-        { 
-           char temp = a;
+        {
+            char temp = a;
             a = b;
-            b = temp;    
+            b = temp;
 
         }
 
@@ -54,6 +79,112 @@ namespace chang.Class_15
             b = temp;
 
         }
+
+
+
+        public void Swap<T>(ref T a, ref T b)
+        {
+
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+        #endregion
+
+
+
+        private void Start()
+        {
+            var player = new Player();
+            var enemy = new Enemy();
+            var attackEvent = new AttackEvent<Player, Enemy>();
+            attackEvent.Attack(player, enemy);
+            var hp = new Hp();
+            var attack = new Attack();
+            hp.Increase(10.5f);
+            attack.Increase(50);
+            hp.Increase(3.75f);
+            var checker = new ChenkValue<Hp, float>();
+            checker.Check(hp);
+
+        }
+
+        #region  泛型類別
+        public class DataPlayer<T>
+        {
+            public T data;
+
+            public void LogData(T data)
+            {
+
+                LogSystem.LogwithColor(data, "3ff");
+
+            }
+
+
+
+        }
+
+
+        public class Player { }
+        public class Enemy { }
+        public class AttackEvent<T, U>
+        {
+            public void Attack(T attacker, U defender)
+            {
+                LogSystem.LogwithColor($"{attacker}攻擊{defender}", "#f3f");
+
+            }
+
+
+        }
+        #endregion
+        #region 泛型介面
+        //泛型介面
+        public  interface Istat<T>
+        {
+            public T value { get; set; }     //該狀態的值
+            public void Increase(T amount);  // 增加該狀態
+
+        }
+        public class Hp : Istat<float>
+        {
+
+            public float value { get; set; }
+            public void Increase(float amount)
+            {
+                value += amount;
+                LogSystem.LogwithColor($"血量:{value}", "#f3f");
+
+            }
+        }
+        public class Attack : Istat<int>
+        {
+
+
+            public int value { get; set; }
+            public void Increase(int amount)
+            {
+                value += amount;
+
+
+                LogSystem.LogwithColor($"攻擊力:{value}", "#f3f");
+            }
+        }
+        #endregion
+
+        //泛型約束:泛型T必須實作IStat<T>介面
+        public class ChenkValue<T,U> where T : Istat<U>
+        {
+            public void Check(T stat)
+            {
+                //添加約束後可以使用IStat<T>成員
+                LogSystem.LogwithColor($"狀態的值:{stat.value}", "$3d3");
+
+            }
+
+        }
+
     }
 }
 
---
+++ Assets/Scripts/Class_16_DataStruct.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections.Generic;            //有許多結構的命名空間
+
+namespace chang.Class_16
+{
+    /// <summary>
+    /// 資料結構
+    /// </summary>
+    public class Class_16_DataStruct : MonoBehaviour
+    {
+        //[陣列
+        public int[] attacks = { 1, 2, 3, };
+        public float[] defens = { 1.5f, 3.5f, 5.5f };
+
+        //清單，泛型
+        public List<int> speeds = new List<int>() { 3, 9, 7 };
+        public List<string> props = new List<string>() { "藥水", "地圖" };
+        public List<float> playerDefens;
+
+        private void Awake()
+        {
+            #region  清單
+            LogSystem.LogwithColor($"第三筆速度:{speeds[2]}", "#3f3");
+            LogSystem.LogwithColor($"第一筆速度:{speeds[0]}", "#3f3");
+            //添加一筆道具
+            props.Add("寶劍");
+            //刪除第一筆道具
+            props.RemoveAt(0);
+            //添加頭盔到編號1上
+            props.Insert(1, "頭盔");
+            foreach (var prop in props)
+            {
+                LogSystem.LogwithColor($"道具:{prop}", "#f93");
+
+            }
+
+            //使用建構子帶入陣列
+            playerDefens = new List<float>(defens);
+            //排序:由小到大
+            playerDefens.Sort();
+            //反排序:由大到小
+            playerDefens.Reverse();
+
+
+            foreach (var item in playerDefens)
+            {
+                LogSystem.LogwithColor($"資料:{item}", "#f93");
+
+            }
+
+
+            //count指清單內的資料，根據Add或Remove改變
+            LogSystem.LogwithColor($"數量:{props.Count}", "#77f");
+            //capcity指清單內的容量，系統自動分配，預設為4並且以倍數成長(C#各版本不同)
+            LogSystem.LogwithColor($"容量:{props.Count}", "#77f");
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                numbers.Add(i);
+                LogSystem.LogwithColor($"數量:{numbers.Count}", "#3a3");
+                LogSystem.LogwithColor($"容量:{numbers.Capacity}", "#f39");
+            }
+
+            #endregion
+            #region  堆疊
+            //堆疊:陷進後出，類似椅子堆在一起
+            Stack<string> enemys = new Stack<string>();
+            //放資料進入堆疊
+            enemys.Push("史萊姆");
+            enemys.Push("哥布林");
+            LogStack<string>(enemys);
+            //拿資料並且不移除
+            enemys.Peek();
+            LogStack<string>(enemys);
+            //拿資料並移除
+            enemys.Pop();
+            LogStack<string>(enemys);
+            //判斷是否包含某筆資料
+            LogSystem.LogwithColor($"{enemys.Contains("哥布林")}", "#3f6");
+            //清除所有資料
+            enemys.Clear();
+            LogStack<string>(enemys);
+
+        }
+        private void LogStack<T>(Stack<T> stack)
+        {
+            LogSystem.LogwithColor("-------------", "#fff");
+            foreach (var item in stack)
+            {
+
+                LogSystem.LogwithColor($"堆疊資料:{item}", "#f77");
+            } 
+            #endregion
+            Queue<string> player = new Queue<string>();
+            player.Enqueue("盜賊");
+            player.Enqueue("法師");
+            player.Enqueue("戰士");
+            LogQueue<string>(player);
+            //拿東西不刪除，與堆疊Peek相同
+            LogSystem.LogwithColor(player.Peek(), "#f33");
+            LogQueue<string>(player);
+            //拿東西並刪除，與堆疊Pop相同
+            LogSystem.LogwithColor(player.Dequeue(), "#f33");
+            LogQueue<string>(player);
+        }
+     
+        private void LogQueue<T> (Queue<T> queue)
+        {
+            LogSystem.LogwithColor("-------------", "#fff");
+
+            foreach (var item in queue)
+            {
+
+                LogSystem.LogwithColor($"佇列資料:{item}", "#7f7");
+            }
+        }
+       
+
+           
+          
+
+
+           
+
+    }  
+}
+
+
+
+
+
+    
+
